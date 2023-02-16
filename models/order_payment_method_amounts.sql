8c66d6a41c4497899d3719be0f5b106b21507f8d
K.Gowtham@ltimindtree.com <K.Gowtham@ltimindtree.com>
2023-02-16 11:04:21 +0000

    done

---
+++ models/order_payment_method_amounts.sql
@@ -0,0 +1,27 @@
+--option 3: variable and Use whitespace control to tidy up compiled code
+{%- set payment_methods = ["bank_transfer", "credit_card", "gift_card"] -%}
+--option 4: marco
+{% set payment_methods = get_payment_methods() %}
+
+select
+order_id,
+
+--option 1: Normal sql
+/*sum(case when payment_method = 'bank_transfer' then amount end) as bank_transfer_amount,
+sum(case when payment_method = 'credit_card' then amount end) as credit_card_amount,
+sum(case when payment_method = 'gift_card' then amount end) as gift_card_amount,*/
+
+--option 2: Use a for loop in models for repeated SQL
+/*{% for payment_method in ["bank_transfer", "credit_card", "gift_card"] %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
+{% endfor%}*/
+
+--option 3: Set variables at the top of a model
+{% for payment_method in payment_methods %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount
+{%- if not loop.last %},{% endif -%}
+{% endfor %}
+--sum(amount) as total_amount
+--from {{ ref('stg_payments') }}
+from {{ source('DBT_PRACTICE', 'stg_payments') }}
+group by 1
